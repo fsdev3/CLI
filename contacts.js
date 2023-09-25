c74939de29fb42fc971fea39b49f72d0933799a2
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-09-25 14:45:14 +0200

    some fixes

--- contacts.js
+++ contacts.js
@@ -5,40 +5,28 @@ import { nanoid } from "nanoid";
 const contactsPath = path.resolve("db", "contacts.json");
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath);
-    const buffer = JSON.parse(data);
-    return buffer;
-  } catch (err) {
-    console.log(err.message);
-  }
+  const data = await fs.readFile(contactsPath);
+  const buffer = JSON.parse(data);
+  return buffer;
 }
 
 async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-    const contactById = contacts.find((cont) => cont.id === contactId);
-    return contactById || null;
-  } catch (err) {
-    console.log(err.message);
-  }
+  const contacts = await listContacts();
+  const contactById = contacts.find((cont) => cont.id === contactId);
+  return contactById || null;
 }
 
 async function removeContact(contactId) {
-  try {
-    const contacts = await listContacts();
-    const contactIndex = contacts.findIndex((cont) => cont.id === contactId);
-    if (contactIndex === -1) {
-      return null;
-    }
-
-    const [result] = contacts.splice(contactIndex, 1);
-
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
-  } catch (error) {
-    throw error;
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex((cont) => cont.id === contactId);
+  if (contactIndex === -1) {
+    return null;
   }
+
+  const [result] = contacts.splice(contactIndex, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
 async function addContact({ name, email, phone }) {
